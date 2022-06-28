d01ea415f76fc519e7a474745870428b166bd93d
Daniil Vorobiov <daniilvorobiov@icloud.com>
2022-06-29 00:21:46 +0300

    урок третий правки

--- script01.js
+++ script01.js
@@ -1,7 +1,27 @@
 let lang = prompt('На каком языке вывести дни недели?');
+if (lang !=="ru" && lang !=="en"){
+    console.log("Что-то пошло не так");
+}
+
+if (lang=="ru"){
+    console.log("понедельник, вторник, среда, четверг, пятница, суббота, воскресенье");
+}
+else if(lang=="en"){
+    console.log("Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday");
+}
+
+switch(true){
+    case lang=="ru":
+        console.log("понедельник, вторник, среда, четверг, пятница, суббота, воскресенье");
+        break;
+    case lang=="en":
+        console.log("Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday");
+        break;
+}
+
 let weekdays = {
     ru: ['понедельник, вторник, среда, четверг, пятница, суббота, воскресенье'],
-    en: ['Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday']
+    en: ['Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday'],
 };
 console.log(weekdays[lang].toString());
 
