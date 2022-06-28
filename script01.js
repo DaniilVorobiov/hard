4b90ae45566461de2cd3f550954dd99c9f084129
Daniil Vorobiov <daniilvorobiov@icloud.com>
2022-06-28 21:08:19 +0300

    урок второй

---
+++ script01.js
@@ -0,0 +1,9 @@
+let number = 266219;
+let arrayNumber = ("" + number).split("").map(Number);
+let sum = 1;
+for (let i = 0; i < arrayNumber.length; i++) {
+    sum *= arrayNumber[i];
+}
+sum = sum ** 3;
+let arraySum = ("" + sum).split("").map(Number);
+console.log(arraySum[0] + '' + arraySum[1]);
