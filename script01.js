39748629c187dd3d36f247fe13dc777b5e768c17
Daniil Vorobiov <daniilvorobiov@icloud.com>
2022-06-30 16:13:46 +0300

    урок 4

--- script01.js
+++ script01.js
@@ -1,15 +1,13 @@
 let argument;
 
 function getArgument() {
-    if(typeof argument != "string"){
+    if (typeof argument != "string") {
         return "Передана не строка"
-    }
-    else if (typeof argument == "string"){
-        if (argument.length>30){
-        argument = argument.slice(0, 29) + "...";
-        return argument.trim()
-        }
-        else return argument.trim()
+    } else if (typeof argument == "string") {
+        if (argument.length > 30) {
+            argument = argument.slice(0, 29) + "...";
+            return argument.trim()
+        } else return argument.trim()
     }
 }
 
