06109a8a9e1d3b06b3f1e0b54b01fb25c2c66251
Daniil Vorobiov <daniilvorobiov@icloud.com>
2022-06-30 16:12:49 +0300

    урок 4

--- script01.js
+++ script01.js
@@ -1,32 +1,17 @@
-let lang = prompt('На каком языке вывести дни недели?');
-if (lang !=="ru" && lang !=="en"){
-    console.log("Что-то пошло не так");
+let argument;
+
+function getArgument() {
+    if(typeof argument != "string"){
+        return "Передана не строка"
+    }
+    else if (typeof argument == "string"){
+        if (argument.length>30){
+        argument = argument.slice(0, 29) + "...";
+        return argument.trim()
+        }
+        else return argument.trim()
+    }
 }
 
-if (lang=="ru"){
-    console.log("понедельник, вторник, среда, четверг, пятница, суббота, воскресенье");
-}
-else if(lang=="en"){
-    console.log("Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday");
-}
-
-switch(true){
-    case lang=="ru":
-        console.log("понедельник, вторник, среда, четверг, пятница, суббота, воскресенье");
-        break;
-    case lang=="en":
-        console.log("Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday");
-        break;
-}
-
-let weekdays = {
-    ru: ['понедельник, вторник, среда, четверг, пятница, суббота, воскресенье'],
-    en: ['Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday'],
-};
-console.log(weekdays[lang].toString());
-
-let namePerson = prompt("Имя");
-let personPosition = namePerson == "Артем" ? "директор" :
-    namePerson == "Александр" ? "преподаватель" :
-    "студент";
-console.log(personPosition);
+argument = prompt("Введите строку");
+console.log(getArgument());
