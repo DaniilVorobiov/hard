af8b46514ecb02df6855df1d8413171abd7c8c07
Daniil Vorobiov <daniilvorobiov@icloud.com>
2022-07-01 19:13:03 +0300

    урок 5

--- script01.js
+++ script01.js
@@ -1,16 +1,16 @@
-let argument;
+let arr = ['101', '20', '3', '50','25', '3000', '584'];
+console.log(arr.filter(num => ['2','4'].includes(num.toString()[0])));
 
-function getArgument() {
-    if (typeof argument != "string") {
-        return "Передана не строка"
-    } else if (typeof argument == "string") {
-        if (argument.length > 30) {
-            argument = argument.slice(0, 29) + "...";
-            return argument.trim()
-        } else return argument.trim()
-    }
+
+let n = 100;
+
+nextPrime:
+for (let i = 0; i <= 6; i++) { // Для всех i...
+
+  for (let j = 2; j < arr[i]; j++) { // проверить, делится ли число..
+    if (arr[i] % j == 0) continue nextPrime; // не подходит, берём следующее
+  }
+
+  console.log( arr[i],"Делители этого числа: 1 и " + arr[i] ); // простое число
 }
 
-argument = prompt("Введите строку");
-
-console.log(getArgument());
